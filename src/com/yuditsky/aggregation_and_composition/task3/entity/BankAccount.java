b0a85a4e172735ec2cb750eb96e4b86827d2664e
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-10-15 10:01:24 +0300

    Override hasCode()/equals()/toString()

--- src/com/yuditsky/aggregation_and_composition/task3/entity/BankAccount.java
+++ src/com/yuditsky/aggregation_and_composition/task3/entity/BankAccount.java
@@ -1,5 +1,7 @@
 package com.yuditsky.aggregation_and_composition.task3.entity;
 
+import java.util.Objects;
+
 public class BankAccount {
     private int number;
     private double amount;
@@ -13,6 +15,30 @@ public class BankAccount {
         this.number = number;
         this.amount = amount;
         this.isBlocked = isBlocked;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "number=" + number +
+                ", amount=" + amount +
+                ", isBlocked=" + isBlocked +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankAccount account = (BankAccount) o;
+        return number == account.number &&
+                Double.compare(account.amount, amount) == 0 &&
+                isBlocked == account.isBlocked;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, amount, isBlocked);
     }
 
     public int getNumber() {
--- src/com/yuditsky/aggregation_and_composition/task3/entity/Client.java
+++ src/com/yuditsky/aggregation_and_composition/task3/entity/Client.java
@@ -1,6 +1,7 @@
 package com.yuditsky.aggregation_and_composition.task3.entity;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Client {
     private ArrayList<BankAccount> bankAccounts;
@@ -15,5 +16,25 @@ public class Client {
 
     public void setBankAccounts(ArrayList<BankAccount> bankAccounts) {
         this.bankAccounts = bankAccounts;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "bankAccounts=" + bankAccounts +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return bankAccounts.equals(client.bankAccounts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bankAccounts);
     }
 }
